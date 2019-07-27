1c7038c0e47fa25c8ac9e1ef4104ee27041a8f35
Dimitar Dimitrov <daghostman.dd@gmail.com>
2019-07-27 22:22:58 +0300

    remove constructor arg for yaml reader

--- src/Config/Reader/YamlReader.php
+++ src/Config/Reader/YamlReader.php
@@ -10,19 +10,12 @@ class YamlReader implements ReaderInterface
         Yaml::PARSE_EXCEPTION_ON_INVALID_TYPE |
         Yaml::PARSE_CUSTOM_TAGS;
 
-    private $options;
-
-    public function __construct(int $configurationYamlOptions = null)
-    {
-        $this->options = $configurationYamlOptions ?? self::DEFAULT_OPTIONS;
-    }
-
     public function parse(string $filename): array
     {
         if (!file_exists($filename)) {
             throw new \InvalidArgumentException("Unable to parse {$filename}");
         }
 
-        return Yaml::parseFile($filename, $this->options);
+        return Yaml::parseFile($filename, static::DEFAULT_OPTIONS);
     }
 }
